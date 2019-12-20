e2764485436103d099b72e42d3182ae62619d83f
tobilastik <rajioluwatobih@gmail.com>
2019-12-20 09:14:53 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
+import {Layout, Header, Navigation, Content} from 'react-mdl';
 import Main from './components/Main';
 import {Link} from 'react-router-dom';
 
--- src/components/Landingpage.js
+++ src/components/Landingpage.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import {Grid, Cell} from 'react-mdl';
 import Confetti from 'react-confetti';
 import TextyAnim from 'rc-texty';
 import Banner from './Banner';
